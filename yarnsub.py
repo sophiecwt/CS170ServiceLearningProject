a11e4efe180391a7f00894637e1547b72e929098
Sadie Walker <sswalkerxo@gmail.com>
2024-12-02 14:00:04 -0500

    Update yarnsub.py

--- yarnsub.py
+++ yarnsub.py
@@ -7,3 +7,26 @@
 
 # formula:
 # (i * h) / j = k
+def yarnSub (h,i,j,k):
+    sub = 0
+    (i * h) / j = k
+    sub = g
+    return(sub)
+
+def main():
+    h = input("number of balls / skeins of the yarn that is recommended in pattern: ")
+    h = float(h)
+    i = input("length of 1 ball / skein of the yarn that is recommended in pattern in meters: ")
+    i = float(i)
+    j = input("length of 1 ball / skein of the desired yarn in meters: ")
+    j = float(j)
+    k = input("number of balls / skeins of the desired yarn required for the pattern: ")
+    k = float(k)
+
+    sub = yarnSub(h,i,j,k)
+    print(sub)
+
+
+# call main when you click run or load this file
+if __name__ == "__main__":
+    main()
