bcc8809f3612e6270af7b5ea38aa47e41afeb38a
Sophie <sophiewahltaylor@gmail.com>
2024-12-02 13:46:27 -0500

    Entered guidlines for other funtions

--- gaugepattern.py
+++ gaugepattern.py
@@ -1,1 +1,9 @@
 #This Is where we will write a gauge function for modifying gauge from a written pattern
+
+#d: Rows/stitches per 10cm in original pattern
+#e: Rows/stitches in original pattern
+#f: Rows/stitches per 10cm inches in desired gauge
+#g: Equivalent rows/stitches in your desired gauge
+
+# formula:
+# g = (e / d) * f
--- yarnsub.py
+++ yarnsub.py
@@ -1,1 +1,9 @@
 #This is where we will write the function to substitute yarns 
+
+# h: number of balls / skeins of the yarn that is recommended in pattern
+# i: length of 1 ball / skein of the yarn that is recommended in pattern in meters
+# j: length of 1 ball / skein of the desired yarn in meters
+# k: number of balls / skeins of the desired yarn required for the pattern
+
+# formula:
+# (i * h) / j = k
